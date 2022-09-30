86ab30ac06d3399ac2644cb697ee888d5540c399
Erick Cruz <erickreis@live.com>
2022-09-30 12:09:26 -0300

    Matriz de distãncias entre neurônios

--- som.m
+++ som.m
@@ -12,7 +12,7 @@ close all;
 %% Parâmetros iniciais
 % parmaters
 number_of_inputs=1000; % 1000
-N=10; % N^2 points 10
+N=5; % N^2 points 10
 
 upper_bound_x=1;
 lower_bound_x=-1;
@@ -77,10 +77,16 @@ end
 MD = zeros(N,N);
 
 %% Matriz de Coordenadas
-MC = cell(10);
+MC = cell(N);
 
 %% Matriz de Trajetórias
-MT = cell(10);
+MT = cell(N);
+
+%% Matriz de Coordenadas dos Neurônios em Cada Atualização
+MN = cell(N);
+
+%% Matriz de Distãncias entre Neurônios
+MDEN = cell(N);
 
 %% Visualização inicial [instante t=0]
 % initial figures
@@ -129,22 +135,17 @@ while (t<=T)
         j1_c= minj1;
         j2_c= minj2;
         
-        % Coordenadas de todos os neurônios INICIAIS
-%         for m=1:N
-%             for n=1:N
-%                 MN(m,n) = {[w1(m,n),w2(m,n)]};
-%             end
-%         end
+        % Coordenadas de todos os neurônios
+        m=0;
+        n=0;
+        for m=1:N
+            for n=1:N
+                MN(m,n) = {[w1(m,n),w2(m,n)]};
+            end
+        end
         
         % coordenada do neurônio vencedor antes da atualização [vencedor]
         cwn1 = [w1(j1_c,j2_c),w2(j1_c,j2_c)]; % coordinate winner neuron 1 [cwn1]
-        
-        % Matriz de distância entre cada neurônio
-%         for mm=1:N
-%             for nn=1:N
-%                 MDEN{j1_c,j2_c} = 
-%             end
-%         end
         
         % Acumulação das coordenadas vencedoras antes da atualização
         if(isempty(MT{j1_c,j2_c}))
@@ -152,11 +153,36 @@ while (t<=T)
         elseif(MT{j1_c,j2_c}{end} ~= cwn1)
             MT{j1_c,j2_c}{end+1} = (cwn1);
         end
+        
+        % Matriz de distância entre cada neurônio
+        ii=0;
+        jj=0;
+        [lin,col] = size(MN);
+        for ii=1:lin
+            for jj=1:col
+                if(MN{ii,jj} ~= cwn1)
+                    if(isempty(MDEN{j1_c,j2_c}))
+                        MDEN{j1_c,j2_c} = norm(cwn1-MN{ii,jj});
+                    else
+                        MDEN{j1_c,j2_c}(end+1) = norm(cwn1-MN{ii,jj});
+                    end
+                end
+            end
+        end
 
         % update the winning neuron
         e_factor = exp(-((j1_c-j1_c).^2+(j2_c-j1_c).^2)/2*sigma);
         w1(j1_c,j2_c)=w1(j1_c,j2_c) + alpha * (x1(i) - w1(j1_c,j2_c));
         w2(j1_c,j2_c)=w2(j1_c,j2_c) + alpha * (x2(i) - w2(j1_c,j2_c));
+        
+        % Coordenadas de todos os neurônios
+        m=0;
+        n=0;
+        for m=1:N
+            for n=1:N
+                MN(m,n) = {[w1(m,n),w2(m,n)]};
+            end
+        end
         
         % coordenada do neurônio vencedor depois da atualização [vencedor]
         cwn2 = [w1(j1_c,j2_c),w2(j1_c,j2_c)]; % coordinate winner neuron 2 [cwn2]
@@ -166,6 +192,22 @@ while (t<=T)
             MT{j1_c,j2_c} = {cwn2};
         elseif(MT{j1_c,j2_c}{end} ~= cwn2)
             MT{j1_c,j2_c}{end+1} = (cwn2);
+        end
+        
+        % Matriz de distância entre cada neurônio
+        ii=0;
+        jj=0;
+        [lin,col] = size(MN);
+        for ii=1:lin
+            for jj=1:col
+                if(MN{ii,jj} ~= cwn1)
+                    if(isempty(MDEN{j1_c,j2_c}))
+                        MDEN{j1_c,j2_c} = norm(cwn1-MN{ii,jj});
+                    else
+                        MDEN{j1_c,j2_c}(end+1) = norm(cwn1-MN{ii,jj});
+                    end
+                end
+            end
         end
         
         % atualiza a coordenada do neurônio [vencedor]
@@ -312,113 +354,113 @@ while (t<=T)
     end
       
 %% plot em intervalo de iterações
-if(t==1)
-    figure(3)
-    subplot(3,3,1)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==10)
-    figure(3)
-    subplot(3,3,2)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==20)
-    figure(3)
-    subplot(3,3,3)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==50)
-    figure(3)
-    subplot(3,3,4)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==100)
-    figure(3)
-    subplot(3,3,5)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==150)
-    figure(3)
-    subplot(3,3,6)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==200)
-    figure(3)
-    subplot(3,3,7)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==250)
-    figure(3)
-    subplot(3,3,8)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)])
-    hold off
-end
-
-if(t==300)
-    figure(3)
-    subplot(3,3,9)
-    plot(x1,x2,'ob')
-    hold on
-    plot(w1,w2,'r','linewidth',1)
-    plot(w1',w2','r','linewidth',1)
-    plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
-    title(['t=' num2str(t)]);
-    hold off
-end
+% if(t==1)
+%     figure(3)
+%     subplot(3,3,1)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==10)
+%     figure(3)
+%     subplot(3,3,2)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==20)
+%     figure(3)
+%     subplot(3,3,3)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==50)
+%     figure(3)
+%     subplot(3,3,4)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==100)
+%     figure(3)
+%     subplot(3,3,5)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==150)
+%     figure(3)
+%     subplot(3,3,6)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==200)
+%     figure(3)
+%     subplot(3,3,7)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==250)
+%     figure(3)
+%     subplot(3,3,8)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)])
+%     hold off
+% end
+% 
+% if(t==300)
+%     figure(3)
+%     subplot(3,3,9)
+%     plot(x1,x2,'ob')
+%     hold on
+%     plot(w1,w2,'r','linewidth',1)
+%     plot(w1',w2','r','linewidth',1)
+%     plot(w1,w2,'yo','MarkerSize',6,'MarkerEdgeColor','r','MarkerFaceColor','y')
+%     title(['t=' num2str(t)]);
+%     hold off
+% end
 
 t=t+1;
 end
